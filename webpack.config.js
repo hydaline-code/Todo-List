e1c49a241f530e96a1a1e08da74f3e06e8f17557
hydaline-code <djougancharlen@.com>
2023-07-22 07:12:24 +0100

    Linters check

--- webpack.config.js
+++ webpack.config.js
@@ -28,6 +28,10 @@ module.exports = {
           'sass-loader',
         ],
       },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
     ],
   },
 };
